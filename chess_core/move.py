52a1ef041c7e2248b2ced81e7e44a43d9226ae3b
NikolaosSopiadis <99847949+NikolaosSopiadis@users.noreply.github.com>
2025-10-10 23:03:31 +0300

    updated move.py

--- chess_core/move.py
+++ chess_core/move.py
@@ -1,22 +1,51 @@
-# Move struct
+from __future__ import annotations
 from dataclasses import dataclass
-from typing import ClassVar
+from enum import IntEnum, IntFlag
 
-@dataclass
+class Promotion(IntEnum):
+    NONE   = 0
+    KNIGHT = 1
+    BISHOP = 2
+    ROOK   = 3
+    QUEEN  = 4
+
+class MoveFlag(IntFlag):
+    QUIET       = 0
+    CAPTURE     = 1 << 0
+    EN_PASSANT  = 1 << 1
+    CASTLE      = 1 << 2
+    PROMOTION   = 1 << 3
+    DOUBLE_PAWN = 1 << 4
+
+@dataclass(frozen=True)
 class Move:
-    # TODO: convert to flags
     src_square: int # idx of source square
     dst_square: int # idx of destination square
 
-    en_passant_capture: bool = False
-    castle: bool             = False
-    pawn_two_up: bool        = False
+    flags: MoveFlag      = MoveFlag.QUIET
+    promotion: Promotion = Promotion.NONE
+    captured: int = 0 # idx of captrued piece
 
-    PROMOTE_TO_NONE: ClassVar[int]   = 0
-    PROMOTE_TO_KNIGHT: ClassVar[int] = 1
-    PROMOTE_TO_BISHOP: ClassVar[int] = 2
-    PROMOTE_TO_ROOK: ClassVar[int]   = 3
-    PROMOTE_TO_QUEEN: ClassVar[int]  = 4
+    @staticmethod
+    def normal(src: int, dst: int, captured: int = 0) -> Move:
+        return Move(src, dst,
+                    MoveFlag.CAPTURE if captured else MoveFlag.QUIET,
+                    Promotion.NONE, captured)
 
-    
-    promotion_type: int = PROMOTE_TO_NONE
+    @staticmethod
+    def promotion_to(src: int, dst: int, to: Promotion, captured: int = 0) -> Move:
+        flags = MoveFlag.PROMOTION
+        if captured: flags |= MoveFlag.CAPTURE
+        return Move(src, dst, flags, to, captured)
+
+    @staticmethod
+    def castle(src: int, dst: int) -> Move:
+        return Move(src, dst, MoveFlag.CASTLE)
+
+    @staticmethod
+    def en_passant(src: int, dst: int) -> Move:
+        return Move(src, dst, MoveFlag.EN_PASSANT | MoveFlag.CAPTURE)
+
+    @staticmethod
+    def double_pawn(src: int, dst: int) -> Move:
+        return Move(src, dst, MoveFlag.DOUBLE_PAWN)
