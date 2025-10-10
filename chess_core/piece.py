d12e9ad81602965a7e40f21e44af7186b9c21679
NikolaosSopiadis <99847949+NikolaosSopiadis@users.noreply.github.com>
2025-10-10 23:19:41 +0300

    marked Piece class fields as final

--- chess_core/piece.py
+++ chess_core/piece.py
@@ -1,37 +1,37 @@
-
+from typing import Final
 
 class Piece:
     # Piece Types
-    NONE: int   = 0
-    PAWN: int   = 1
-    KNIGHT: int = 2
-    BISHOP: int = 3
-    ROOK: int   = 4
-    QUEEN: int  = 5
-    KING: int   = 6
+    NONE: Final[int]   = 0
+    PAWN: Final[int]   = 1
+    KNIGHT: Final[int] = 2
+    BISHOP: Final[int] = 3
+    ROOK: Final[int]   = 4
+    QUEEN: Final[int]  = 5
+    KING: Final[int]   = 6
  
     # Piece Colours
-    WHITE: int = 0
-    BLACK: int = 8
+    WHITE: Final[int] = 0
+    BLACK: Final[int] = 8
 
     # Pieces
-    WHITE_PAWN: int   = PAWN   | WHITE # 1
-    WHITE_KNIGHT: int = KNIGHT | WHITE # 2
-    WHITE_BISHOP: int = BISHOP | WHITE # 3
-    WHITE_ROOK: int   = ROOK   | WHITE # 4
-    WHITE_QUEEN: int  = QUEEN  | WHITE # 5
-    WHITE_KING: int   = KING   | WHITE # 6
+    WHITE_PAWN: Final[int]   = PAWN   | WHITE # 1
+    WHITE_KNIGHT: Final[int] = KNIGHT | WHITE # 2
+    WHITE_BISHOP: Final[int] = BISHOP | WHITE # 3
+    WHITE_ROOK: Final[int]   = ROOK   | WHITE # 4
+    WHITE_QUEEN: Final[int]  = QUEEN  | WHITE # 5
+    WHITE_KING: Final[int]   = KING   | WHITE # 6
 
-    BLACK_PAWN: int   = PAWN   | BLACK # 9
-    BLACK_KNIGHT: int = KNIGHT | BLACK # 10
-    BLACK_BISHOP: int = BISHOP | BLACK # 11
-    BLACK_ROOK: int   = ROOK   | BLACK # 12
-    BLACK_QUEEN: int  = QUEEN  | BLACK # 13
-    BLACK_KING: int   = KING   | BLACK # 14
+    BLACK_PAWN: Final[int]   = PAWN   | BLACK # 9
+    BLACK_KNIGHT: Final[int] = KNIGHT | BLACK # 10
+    BLACK_BISHOP: Final[int] = BISHOP | BLACK # 11
+    BLACK_ROOK: Final[int]   = ROOK   | BLACK # 12
+    BLACK_QUEEN: Final[int]  = QUEEN  | BLACK # 13
+    BLACK_KING: Final[int]   = KING   | BLACK # 14
     
     # Bit Masks
-    TYPE_MASK: int  = 0b0111
-    COLOR_MASK:int = 0b1000
+    TYPE_MASK: Final[int]  = 0b0111
+    COLOR_MASK:Final[int] = 0b1000
 
     @staticmethod
     def make_piece(piece_type: int, piece_color: int) -> int:
