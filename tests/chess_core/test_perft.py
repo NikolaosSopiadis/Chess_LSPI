486e5a7cf661030b95edda3ea6773fce423bbebc
NikolaosSopiadis <99847949+NikolaosSopiadis@users.noreply.github.com>
2026-01-11 18:20:24 +0200

    additional perf tests

--- tests/chess_core/test_perft.py
+++ tests/chess_core/test_perft.py
@@ -36,6 +36,30 @@ def test_perft_pos2(depth, expected):
     assert perft(b, depth) == expected
 
 @pytest.mark.parametrize("depth,expected", [
+    (1, 14),
+    (2, 191),
+    (3, 2812),
+    (4, 43238),
+    (5, 674624),
+    (6, 11030083),
+])
+def test_perft_pos3(depth, expected):
+    b = Board()
+    b.set_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 ")
+    assert perft(b, depth) == expected
+
+@pytest.mark.parametrize("depth,expected", [
+    (1, 6),
+    (2, 264),
+    (3, 9467),
+    (4, 422333),
+])
+def test_perft_pos4(depth, expected):
+    b = Board()
+    b.set_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")
+    assert perft(b, depth) == expected
+
+@pytest.mark.parametrize("depth,expected", [
     (1, 44),
     (2, 1486),
     (3, 62379),
@@ -44,4 +68,15 @@ def test_perft_pos2(depth, expected):
 def test_perft_pos5(depth, expected):
     b = Board()
     b.set_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ")
+    assert perft(b, depth) == expected
+    
+@pytest.mark.parametrize("depth,expected", [
+    (1, 46),
+    (2, 2079),
+    (3, 89890),
+    (4, 3894594),
+])
+def test_perft_pos6(depth, expected):
+    b = Board()
+    b.set_fen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10")
     assert perft(b, depth) == expected
